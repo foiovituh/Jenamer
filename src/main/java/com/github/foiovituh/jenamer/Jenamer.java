3e3684569faccb94695c2909efa29fb34769b170
Vituh <vitorsilvaalvescontato@gmail.com>
2022-04-18 12:31:42 -0300

    simplify argument constant name

--- src/main/java/com/github/foiovituh/jenamer/Jenamer.java
+++ src/main/java/com/github/foiovituh/jenamer/Jenamer.java
@@ -4,10 +4,10 @@ import com.github.foiovituh.jenamer.io.Renamer;
 import com.github.foiovituh.jenamer.validation.Validator;
 
 public final class Jenamer {
-    private static final int CURRENT_NUMBER_OF_ARGUMENTS_REQUIRED = 3;
+    private static final int NECESSARY_ARGUMENTS = 3;
     
     public static void main(String[] args) {
-        Validator.arguments(args, CURRENT_NUMBER_OF_ARGUMENTS_REQUIRED);
+        Validator.arguments(args, NECESSARY_ARGUMENTS);
         new Renamer(args[0]).renameAllFilesTo(args[1], args[2]);
     }
 }
